e8e8b3098000baa34ef75c6cd1cc14fea8abaced
NedimNuhovic <nedimnuhovic54@gmail.com>
2026-02-04 17:03:03 +0100

    1+3

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(a,b):
 
+    print(f'{a}+{b}={a+b}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hox')
+    print_hi(1,3)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
