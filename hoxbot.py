063013ec6f8598812e791d9a683f7b5ef03b4b8d
NedimNuhovic <nedimnuhovic54@gmail.com>
2026-02-10 13:22:05 +0100

    Added AI prompt feature using ollama

--- hoxbot.py
+++ hoxbot.py
@@ -4,12 +4,12 @@ import logging
 import os
 from dotenv import load_dotenv
 import pyodbc
+import ollama
+import asyncio
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
-
 message_logger = logging.getLogger('message_logger')
 message_logger.setLevel(logging.INFO)
 message_handler = logging.FileHandler("message.log", encoding="utf-8", mode='a')
@@ -24,7 +24,8 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='hox', intents=intents)
 bot_channel_id = 1466365993981972598
-dollie_server_id = 1400768510636068887
+
+client = ollama.Client(host="http://127.0.0.1:11434")
 connection = (
         "DRIVER={ODBC Driver 17 for SQL Server};"
         "SERVER=.\\SQLEXPRESS;"
@@ -38,19 +39,16 @@ def get_response_for_text(text: str):
     conn = pyodbc.connect(connection)
     cur = conn.cursor()
     cur.execute("""
-        SELECT [trigger_word], [response]
-        FROM dbo.response_table    
-    """)
+        SELECT TOP (1) response
+        FROM dbo.response_table
+        WHERE ? LIKE '%' + trigger_word + '%'
+        ORDER BY LEN(trigger_word) DESC
+    """, (text.lower(),))
 
-    text = text.lower()
-
-    for trigger_word, response_word in cur.fetchall():
-        if trigger_word.lower() in text:
-            conn.close()
-            return response_word
-
+    row = cur.fetchone()
     conn.close()
-    return None
+
+    return row[0] if row else None
 
 async def insert_trigger_response(trigger_word, response_word, ctx):
     conn = pyodbc.connect(connection)
@@ -69,7 +67,7 @@ async def remove_trigger_response(trigger_word, ctx):
     cur = conn.cursor()
     cur.execute(
         "DELETE FROM dbo.response_table WHERE trigger_word = ?",
-        trigger_word
+        (trigger_word,)
     )
     conn.commit()
     if cur.rowcount > 0:
@@ -106,7 +104,7 @@ async def on_message(message):
 @bot.command()
 async def add(ctx):
     words = ctx.message.content.split(" | ")
-    trigger_word = words[1]
+    trigger_word = words[1].lower()
     response_word = words[2]
     await insert_trigger_response(trigger_word, response_word, ctx)
 
@@ -118,10 +116,37 @@ async def clear(ctx):
     print(f"trigger={trigger_word}")
     await remove_trigger_response(trigger_word, ctx)
 
+@bot.command()
+async def er(ctx):
+    message = ctx.message.content[6:]
+    print(message)
+    SYSTEM_PROMPT = (
+    "You are upbeat and energetic"
+    "Try to replicate Tohru Adachi from persona 4."
+    "DO NOT SAY THAT YOU ARE ADACHI FROM PERSONA 4."
+    "YOU ARE NOT A DETECTIVE YOU JUST HAVE HIS PERSONALITY"
+    "You love big breasts but only say that if asked"
+    "No explanations. No emojis. Max 50 words."
+    )
+
+    def run_llama():
+        r = client.chat(
+            model="llama3.1:8b",
+            messages=[{"role": "system", "content": SYSTEM_PROMPT},
+                    {"role": "user", "content": f"Everything written in the brackets"
+                    f"will be the message you will respond to ({message})"}
+            ],
+        )
+        return r["message"]["content"].strip()
+
+    reply = await asyncio.to_thread(run_llama)
+
+    await ctx.reply(reply)
+
+
 @bot.event
 async def on_ready():
-    # dollie_general = bot.get_channel(dollie_server_id)
-    # await dollie_general.send("yes")
+
     channel = bot.get_channel(bot_channel_id)
     print(f'Yokai! {bot.user.name} yoroshiku!')
     await channel.send("Yokai!")
